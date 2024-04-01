6b370353785c90191a8389ae7c92258224f97e6e
cafalchio <mcafalchio@gmail.com>
2024-04-01 14:07:04 +0100

    removed django

--- cryptofinder/cryptofinder/coingecko_list.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = "https://api.coingecko.com/api/v3/coins/list"
-headers = {"x-cg-pro-api-key": "API-KEY"}
-response = requests.get(url, headers=headers)
-print(len((response.text).split(",")))
