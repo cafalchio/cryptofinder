429e3b9d3ba08c50d489a4f15efa385a5d0a5b08
cafalchio <mcafalchio@gmail.com>
2024-04-06 17:11:30 +0100

    last piece of commit

--- app/__int__.py
+++
@@ -1,9 +0,0 @@
-from app import routes
-from flask import Flask
-import logging
-
-app = Flask(__name__)
-
-app.logger.setLevel(logging.INFO)
-handler = logging.FileHandler("app.log")
-app.logger.addHandler(handler)
