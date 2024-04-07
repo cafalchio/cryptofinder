b57a1e1d596dde3f5d7f58c1b63d4dc8dcb507af
cafalchio <mcafalchio@gmail.com>
2024-04-07 15:38:09 +0100

    change app to run

--- app/utils.py
+++ app/utils.py
@@ -2,7 +2,7 @@ import os
 import requests
 import pandas as pd
 from datetime import datetime
-from .app import logger
+from app.run import logger
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
 
