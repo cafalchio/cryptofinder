79691e33c1a57c6d53a7950efc52a0cc2aa36412
cafalchio <mcafalchio@gmail.com>
2024-11-02 18:48:09 +0000

    started to save forum text for future name extractor

--- backend/scrappers/btc_talk/btc_talk.py
+++ backend/scrappers/btc_talk/btc_talk.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import logging
+from pprint import pprint
 
 from backend.utils.scrappers import scrap_website_driver
 
@@ -13,6 +14,10 @@ logger = logging.getLogger("__name__")
 DEBUG = True
 
 
+def extract_name(line):
+    name = line.strip(":")
+
+
 def btc_talk():
     today_lines = []
     with scrap_website_driver("https://bitcointalk.org/") as driver:
@@ -20,20 +25,22 @@ def btc_talk():
         alts = WebDriverWait(driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, coins_page))
         )
-        time.sleep(5)
+        time.sleep(3)
         alts.click()
-        time.sleep(10)
+        time.sleep(3)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         trs = soup.find_all("tr")
         for tr in trs:
             if "Today" in tr.text:
-                line = [td.text.strip() for td in tr.find_all("td")]
-                today_lines.append(line)
+                line = [td.text.strip() for td in tr.find_all("td")][2]
+                if "[ANN] " in line and "»" not in line:
+                    today_lines.append(line.split("[ANN] ")[1].strip())
+
     return today_lines
 
 
 if __name__ == "__main__":
     todays = btc_talk()
-    for today in todays:
-        if today:
-            print(f"{today[2]}")
+    with open("btc_text", "a") as f:
+        for line in todays:
+            f.write(line + "\n")
