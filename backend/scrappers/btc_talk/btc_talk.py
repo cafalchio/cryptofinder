d7701a7f65b2db35c945f682623f46ef6225c91f
cafalchio <mcafalchio@gmail.com>
2024-11-03 17:38:14 +0000

    removed 2 python versions, tests should pass

--- backend/scrappers/btc_talk/btc_talk.py
+++ backend/scrappers/btc_talk/btc_talk.py
@@ -3,7 +3,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import datetime
 import logging
 
 from backend.utils.scrappers import scrap_website_driver
