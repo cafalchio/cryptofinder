f604926736920651d9da3906de178aff61bdb732
cafalchio <mcafalchio@gmail.com>
2024-11-03 21:19:45 +0000

    fix imports

--- backend/scrappers/run_scrappers.py
+++ backend/scrappers/run_scrappers.py
@@ -1,26 +1,27 @@
 from app.app import create_app, db
 from backend.data.models import AllCoins
 from sqlalchemy import select
+from app.config_app import DATABASE
 
 
 def run_scrappers():
     # Avoid circular import
     from coingecko import coingecko
-    # from backend.scrappers.exchanges import xeggex
-    # from pools import rplant_xyz, miningpoolstats
+    from backend.scrappers.exchanges import xeggex
+    from pools import rplant_xyz, miningpoolstats
 
     # For now, add the list of functions here
-    # rplant_xyz.rplant()
+    rplant_xyz.rplant()
     coingecko.coingecko()
-    # miningpoolstats.mining_pool_stats()
-    # xeggex.xeggex()
-    # rplant_xyz.rplant()
+    miningpoolstats.mining_pool_stats()
+    xeggex.xeggex()
+    rplant_xyz.rplant()
 
 
 def update_all_coins(coins):
     if not coins:
         return
-    app = create_app()
+    app = create_app(DATABASE)
     with app.app_context():
         existing_all_coins = {
             coin.id for coin in db.session.execute(select(AllCoins)).scalars().all()
@@ -31,7 +32,8 @@ def update_all_coins(coins):
             if id in existing_all_coins:
                 continue
             to_update.append(
-                AllCoins(id=coin.id, symbol=coin.symbol, name=coin.name, is_shit=False)
+                AllCoins(id=coin.id, symbol=coin.symbol,
+                         name=coin.name, is_shit=False)
             )
         if to_update:
             db.session.bulk_save_objects(to_update)
