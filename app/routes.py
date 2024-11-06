cd8ed67f3abdebaa3ad416db2789c27a3c3e23dc
cafalchio <mcafalchio@gmail.com>
2024-11-06 22:10:20 +0000

    show latest coins first

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,33 @@
 from flask import render_template
 from backend.data.models import AllCoins
 from datetime import datetime, timedelta
+from sqlalchemy import desc
 
 
 def register_routes(app, db):
-    time_to_display = datetime.now() - timedelta(days=1)
+    time_to_display = datetime.now() - timedelta(hours=24)
 
     @app.route("/")
     def new_coins_today():
         result = db.session.execute(
             db.select(AllCoins)
             .filter(AllCoins.added > time_to_display)
-            .order_by(AllCoins.added)
+            .order_by(desc(AllCoins.added))
         )
         new_coins = result.scalars().all()  # Get the list of AllCoins
         return render_template("new_coins_today.html", coins=new_coins)
 
     @app.route("/all_coins")
     def all_coins():
-        result = db.session.execute(db.select(AllCoins).order_by(AllCoins.added))
+        result = db.session.execute(
+            db.select(AllCoins).order_by(AllCoins.added))
         all_coins = result.scalars().all()  # Get the list of AllCoins
         return render_template("all_coins.html", coins=all_coins)
 
     @app.route("/shitcoins")
     def shitcoins():
-        result = db.session.execute(db.select(AllCoins).filter(AllCoins.is_shit))
+        result = db.session.execute(
+            db.select(AllCoins).filter(AllCoins.is_shit))
         # Get the list of AllCoins that are "shitcoins"
         shitcoins = result.scalars().all()
         return render_template("shitcoins.html", coins=shitcoins)
