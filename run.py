1204dbce1047d457a31698beef98baebb5c3c108
cafalchio <mcafalchio@gmail.com>
2024-11-08 21:51:16 +0000

    update logger for run file

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 from app.app import create_app
-from app.config_app import DATABASE
+from app.config_app import get_logger
 
+PORT = 10000
+DEBUG = False
+HOST = "0.0.0.0"
 
+logger = get_logger()
 flask_app = create_app()
 
-
 if __name__ == "__main__":
-    flask_app.run(host="0.0.0.0", debug=True, port=10000)
+    bar = {'-'*30}
+    logger.info(f"{bar}\nRunning Server: {HOST}:{PORT} Debug: {DEBUG}\n{bar}")
+    flask_app.run(host=HOST, debug=DEBUG, port=PORT)
