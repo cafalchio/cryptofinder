10d5ff45f6a0407968f29a8b47b7bbd1345afcee
cafalchio <mcafalchio@gmail.com>
2024-11-10 18:58:20 +0000

    added xeggex to scrappers class

--- backend/scrappers/exchanges/xeggex.py
+++ backend/scrappers/exchanges/xeggex.py
@@ -8,19 +8,20 @@ from backend.utils.utils import update_all_coins
 logger = logging.getLogger(__name__)
 
 
-def xeggex():
+def xeggex(self):
+    scrap_config = self.config.scrappers["xeggex"]
+
     new_coins = {}
     pattern = re.compile(r"Announcing the New Listing of (.*) \((.*)\)")
 
-    with scrap_website_soup("https://xeggex.com/news") as soup:
+    with scrap_website_soup(scrap_config["url"]) as soup:
         matches = re.findall(pattern, soup.text)
         if not matches:
             return
         for match in matches:
             name, symbol = match
             new_coins[name] = AllCoins(id=name, symbol=symbol, name=name, is_shit=False)
-    logger.info(f"{'-'*30}\nGot {len(new_coins.keys())} coind from rplantxyz")
-    update_all_coins(new_coins)
+    self.update_all_coins(new_coins)
 
 
 if __name__ == "__main__":
--- backend/scrappers/scrapper_runner.py
+++ backend/scrappers/scrapper_runner.py
@@ -1,6 +1,7 @@
 from app.config_app import get_logger
 from backend.scrappers.btc_talk.btc_talk import BtcTalk
 from backend.scrappers.coingecko.coingecko import Coinbase
+from backend.scrappers.exchanges.xeggex import Xeggex
 from backend.scrappers.pools.miningpoolstats import MiningPoolStats
 from backend.scrappers.pools.rplant_xyz import Rplant
 
@@ -9,10 +10,11 @@ logger = get_logger()
 
 def run_scrappers(config):
     _scrappers = {
-        # "rplant": Rplant,
-        # "coinbase": Coinbase,
-        # "miningpoolstats": MiningPoolStats,
+        "rplant": Rplant,
+        "coinbase": Coinbase,
+        "miningpoolstats": MiningPoolStats,
         "btc_talk": BtcTalk,
+        "xeggex": Xeggex
     }
     for name, scrap_class in _scrappers.items():
         logger.info(f"Running scrapper {name}")
