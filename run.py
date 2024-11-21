1a947d5cbc47cafbe77b3a353b5b9adaaf3d021b
cafalchio <mcafalchio@gmail.com>
2024-11-21 10:40:06 +0000

    changed run.py

--- run.py
+++ run.py
@@ -12,20 +12,26 @@ def config_app(env):
     return flask_app, config
 
 
+def run_scrappers(config):
+    print("\n---------- Running Scrapper ----------\n")
+    for scrapper_name in config.scrappers.keys():
+        scrapper_class = getattr(scrappers, scrapper_name)
+        if config.scrappers[scrapper_name]["enabled"]:
+            print(f"Running: {scrapper_class.__name__}")
+            scrapper = scrapper_class(config)
+            scrapper.run()
+
+
 if __name__ == "__main__":
-    if "prod" in sys.argv:
+    env = "DEV" if "dev" in sys.argv else "PROD"
+    scrap = "scrappers" in sys.argv or "scrapper" in sys.argv
+    if env == "PROD":
         flask_app, config = config_app("PROD")
     else:
         flask_app, config = config_app("DEV")
+        print(f"{config.HOST}:{config.PORT} - DEBUG: {config.DEBUG}")
+    if scrap:
+        run_scrappers(config)
 
-    if "scrappers" in sys.argv or "scrapper" in sys.argv:
-        print("\n---------- Running Scrapper ----------\n")
-        for scrapper_name in config.scrappers.keys():
-            scrapper_class = getattr(scrappers, scrapper_name)
-            if config.scrappers[scrapper_name]["enabled"]:
-                print(f"Running: {scrapper_class.__name__}")
-                scrapper = scrapper_class(config)
-                scrapper.run()
-
-    print(f"{config.HOST}:{config.PORT} - DEBUG: {config.DEBUG}")
+    print(f"Running {env}")
     flask_app.run(host=config.HOST, debug=config.DEBUG, port=config.PORT)
