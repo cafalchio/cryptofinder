6a8e842d5e9d8f699f3a943856d77a83892b7341
cafalchio <mcafalchio@gmail.com>
2024-11-21 11:04:40 +0000

    updated run.py

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ from app.app import create_app, ConfigApp
 import sys
 from backend import scrappers
 
-
 CONFIG_FILE = "environments.json"
 
 
@@ -22,16 +21,20 @@ def run_scrappers(config):
             scrapper.run()
 
 
+# Initialize the app
+env = "DEV" if "dev" in sys.argv else "PROD"
+scrap = "scrappers" in sys.argv or "scrapper" in sys.argv
+
+if env == "PROD":
+    flask_app, config = config_app("PROD")
+else:
+    flask_app, config = config_app("DEV")
+    print(f"{config.HOST}:{config.PORT} - DEBUG: {config.DEBUG}")
+
+app = flask_app
+
 if __name__ == "__main__":
-    env = "DEV" if "dev" in sys.argv else "PROD"
-    scrap = "scrappers" in sys.argv or "scrapper" in sys.argv
-    if env == "PROD":
-        flask_app, config = config_app("PROD")
-    else:
-        flask_app, config = config_app("DEV")
-        print(f"{config.HOST}:{config.PORT} - DEBUG: {config.DEBUG}")
     if scrap:
         run_scrappers(config)
-
     print(f"Running {env}")
     flask_app.run(host=config.HOST, debug=config.DEBUG, port=config.PORT)
