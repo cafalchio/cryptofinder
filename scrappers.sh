4670fbbab6b3f31e443d34465d0fc632572f1a34
cafalchio <mcafalchio@gmail.com>
2024-11-24 09:15:38 +0000

    added date tp scrapper bash

--- scrappers.sh
+++ scrappers.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
 # Activate the virtual environment and run the Python script
+echo date
 source /home/cafalchio/cryptofinder/.venv/bin/activate
 /home/cafalchio/cryptofinder/.venv/bin/python /home/cafalchio/cryptofinder/scrappers.py
