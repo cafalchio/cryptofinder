451b6b0dc3690120f4c6941df66c7cb25af016e3
cafalchio <mcafalchio@gmail.com>
2024-11-30 21:24:16 +0000

    updated btc_talk scrapper

--- backend/scrappers/btc_talk/btc_talk.py
+++ backend/scrappers/btc_talk/btc_talk.py
@@ -5,8 +5,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from app.app import get_logger
 from backend.data.models import AllCoins
 from backend.utils.scrappers import BaseScrapper, scrap_website_driver
+import re
 
 logger = get_logger()
+
+EXCLUDE = ["POW", "POS", "BETA", "PRE", "ANN", "ICO", "All"]
 
 
 class BtcTalk(BaseScrapper):
@@ -30,29 +33,42 @@ class BtcTalk(BaseScrapper):
                     line = [td.text.strip() for td in tr.find_all("td")][2]
                     with open("btc_talk_data", "a") as f:
                         f.write(line)
-                    if "[ANN]" in line and "»" not in line:
-                        name, symbol = self.extract_name(line)
-                        new_coins[name] = AllCoins(
-                            id=name.lower(),
-                            symbol=symbol,
-                            name=name,
-                            source="btc talk",
-                            is_shit=False,
-                        )
-                        line = line.split("ANN]")[1].strip()
-                        today_lines.append(line)
-                        logger.info(line)
+                    if "ANN" not in line or "Topics" in line:
+                        continue
+                    line = clean_text(line)
+                    name, symbol = extract(line)
+                    name = name.replace(symbol, "")
+                    try:
+                        name = " ".join(name.split(" ")[0:3])
+                    except:
+                        name = line
+                    new_coins[name] = AllCoins(
+                        id=name.lower(),
+                        symbol=symbol,
+                        name=name,
+                        source="btc talk",
+                        is_shit=False,
+                    )
             self.update_all_coins(new_coins)
         return today_lines
 
-    def extract_name(self, line):
-        line = line[6:]
-        symbol = ""
-        if ":" in line:
-            name = line.split(":")[0]
-        elif "-" in line:
-            name = line.split("-")[0]
-        else:
-            name = line.split(" ")[0]
-            symbol = line.split(" ")[1]
-        return name, symbol
+
+def clean_text(text):
+    for word in EXCLUDE:
+        text = text.replace(word, " ")
+    text = re.sub(r'[^\x00-\x7F]', '', text)
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    text = text.replace("  ", " ").strip().lstrip()
+    return text
+
+
+def extract(line):
+    name = ""
+    symbol = ""
+    symbol_re = r'\b[A-Z]{3,5}\b'
+    group = re.findall(symbol_re, line)
+    symbol = next((g for g in group if g not in EXCLUDE), "")
+    name = line.replace(symbol, "")
+    name = name.strip()
+    symbol = symbol.strip()
+    return name, symbol
