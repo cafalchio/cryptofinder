35c2d8aa79b13861a3634765b66bda6292c22d1f
cafalchio <mcafalchio@gmail.com>
2024-12-10 07:48:21 +0000

    change coincarp to get just pow coins

--- backend/scrappers/coincarp/coincarp.py
+++ backend/scrappers/coincarp/coincarp.py
@@ -12,19 +12,20 @@ class CoinCarp(BaseScrapper):
         if not scrap_config["enabled"]:
             return
         new_coins = {}
-        with scrap_website_soup(scrap_config["url"]) as soup:
-            divs = soup.find_all("div", class_="name")
-            for div in divs:
-                try:
-                    name = div.find("span", class_="fullname").text.strip()
-                    symbol = div.find("span", class_="symbo").text.strip()
-                except TypeError:
-                    continue
-                new_coins[name] = AllCoins(
-                    id=name.lower(),
-                    symbol=symbol,
-                    name=name,
-                    source=self.name,
-                    is_shit=False,
-                )
+        for url in scrap_config["url"]:
+            with scrap_website_soup(url) as soup:
+                divs = soup.find_all("div", class_="name")
+                for div in divs:
+                    try:
+                        name = div.find("span", class_="fullname").text.strip()
+                        symbol = div.find("span", class_="symbo").text.strip()
+                    except TypeError:
+                        continue
+                    new_coins[name] = AllCoins(
+                        id=name.lower(),
+                        symbol=symbol,
+                        name=name,
+                        source=self.name,
+                        is_shit=False,
+                    )
         self.update_all_coins(new_coins)
